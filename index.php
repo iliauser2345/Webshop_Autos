5bd3b78c8bce559771d51b8f4d9701797c6c235a
iliauser2345 <124836886+iliauser2345@users.noreply.github.com>
2025-12-09 13:21:33 +0100

    /body/main car list

--- index.php
+++ index.php
@@ -9,7 +9,12 @@
         <?php require "./includes/header.php"?>
         
         <main>
-
+            <ul>
+                <li class="auto"><a href="./The_Car/car1.php">[CAR 1]</a><img src="./includes/images/dumdum.jpg" alt="car_image"></li>
+                <li class="auto"><a href="./The_Car/car2.php">[CAR 2]</a><img src="./includes/images/dumdum.jpg" alt="car_image"></li>
+                <li class="auto"><a href="./The_Car/car3.php">[CAR 3]</a><img src="./includes/images/dumdum.jpg" alt="car_image"></li>
+                <li class="auto"><a href="./The_Car/car4.php">[CAR 4]</a><img src="./includes/images/dumdum.jpg" alt="car_image"></li>
+            </ul>
         </main>
         <?php require "./includes/footer.php"?>
     </body>
