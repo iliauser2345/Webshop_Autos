9417b403fbb98c378c96761f2f859555dbae1be7
iliauser2345 <124836886+iliauser2345@users.noreply.github.com>
2025-12-09 12:28:53 +0100

    HEADER en navbar

---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<header class="head&navbar">
+    <h1>[Title van de webshop hier aub]</h1>
+    <nav>
+        <ul>
+            <li><a href="./index.php">Home</a></li>
+            <li><a href=""> [link naar pagina 2 hier]</a></li>
+            <li><a href=""> [link naar pagina 3 hier]</a></li>
+            <li><a href=""> [link naar pagina 4 hier]</a></li>
+            <li><a href=""> [link naar pagina 5 hier]</a></li>
+        </ul>
+    </nav>
+</header>
--- index.php
+++ index.php
@@ -1,3 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Webshop Home</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="./style.css">
+    </head>
+    <body>
+        <?php require "./includes/header.php"?>
+        
+        <main>
+
+        </main>
+    </body>
+</html>
+
+
 <?
 
 echo "hallo";
