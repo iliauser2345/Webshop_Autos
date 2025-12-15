05b8a8792453b56b98b88a8082b85974efc84062
iliauser2345 <124836886+iliauser2345@users.noreply.github.com>
2025-12-15 14:20:44 +0100

    login fix

--- login_data_processing.php
+++ login_data_processing.php
@@ -1,12 +1,14 @@
 <?php
 require 'db.php';
 session_start();
+$login_status=false;
 $gebruikersnaam=$_POST['username'];
 $wachtwoord=$_POST['password'];
     for($i=0;count($users);$i++){
         if($users[$i]['gebruikersnaam']===$gebruikersnaam || $users[$i]['wachtwoord']===$wachtwoord){
            // echo "Logged in as ".$users[$i]['gebruikersnaam'];
             $_SESSION['admin']=$users[$i]['admin_status'];
+            $login_status=true;
             header("Location:index.php");
             exit();
         }
