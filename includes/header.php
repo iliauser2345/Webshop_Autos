c233020b5ee9c09b469c3213678d84938e1e3004
iliauser2345 <124836886+iliauser2345@users.noreply.github.com>
2025-12-16 14:18:27 +0100

    login/out FIXED FFS

--- includes/header.php
+++ includes/header.php
@@ -3,7 +3,6 @@
     <nav>
         <ul>
             <?php
-            try{ 
                 if($_SESSION['logged_in']==true){
                     echo
                     "<li>
@@ -11,26 +10,24 @@
                         <button type='submit' action='logout.php'>Uitloggen</button>
                     </form>
                     </li>";
-                }}catch(Exception $a){
-                    echo " ";
                 }
             ?>
             <li><a href="./index.php">Home</a></li>
             <li><a href="./inlog_pagina.php">
-                <?php try{
-                            if($_SESSION['admin']==1 or $_SESSION['admin']==0){
+                <?php
+                           if($_SESSION['logged_in']==true){
                                 echo "Mijn Cabinet";
-                                $_SESSION['logged_in']=true;
+                                
+                            }elseif($_SESSION['logged_in']==false){
+                                echo 'Inloggen';
                             }
-                        }catch(Exception $b){
-                            echo "Inloggen";}?>
+                        ?>
             </a></li>
             <li><a href=""> Winkelmand</a></li>
-            <?php try{
-                    if ($_SESSION['admin']==1){
-                        echo 'admin';}
-                    }catch(Exception $i){
-                        echo ' ';}?>
+            <?php
+                if ($_SESSION['admin']==1 && $_SESSION['logged_in']==true){
+                    echo 'admin';}?>
+                    
         </ul>
     </nav> 
 </header>
--- login_data_processing.php
+++ login_data_processing.php
@@ -1,12 +1,11 @@
 <?php
 require 'db.php';
 session_start();
-$login_status=false;
 $gebruikersnaam=$_POST['username'];
 $wachtwoord=$_POST['password'];
     for($i=0;count($users);$i++){
         if($users[$i]['gebruikersnaam']===$gebruikersnaam || $users[$i]['wachtwoord']===$wachtwoord){
-           // echo "Logged in as ".$users[$i]['gebruikersnaam'];
+            $_SESSION['logged_in']=true;
             $_SESSION['admin']=$users[$i]['admin_status'];
             header("Location:index.php");
             exit();
